5e0a5bdc16575fb35f9bea45fc1b395877ec4580
Tech <madhav.sharma@axisrooms.com>
2018-03-22 17:44:23 +0530

    UI modifications

--- src/app/support/components/form/billing/billing.component.ts
+++ src/app/support/components/form/billing/billing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormService } from '../../../services/form.service';
 import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { FormDataService } from '../services/form-data.service';
@@ -20,19 +20,19 @@ export class BillingComponent implements OnInit {
     private formDataService: FormDataService,
   ) { }
 
- 
+
 
   ngOnInit() {
     this.billingForm = this.formBuilder.group({
-      productType: ['BE'], 
+      productType: ['BE'],
       transactionCurrency: ['INR'],
       ruleDetails: this.formBuilder.array([
         this.initRulesArray(),
       ])
     });
 
-    this.formDataService.isDefault.subscribe(res => { this.isDefault = res; if(this.isDefault != "DEFAULT") this.productChoice("BE")});
-   
+    this.formDataService.isDefault.subscribe(res => { this.isDefault = res; if (this.isDefault != "DEFAULT") this.productChoice("BE") });
+
   }
   initRulesArray() {
     return this.formBuilder.group({
@@ -46,15 +46,29 @@ export class BillingComponent implements OnInit {
       paymentCycle: [1, Validators.required] //paymentCycle - num 1,3,6,12
     });
   }
-  addRulesForm() {
-    const control = <FormArray>this.billingForm.controls['ruleDetails'];1
-    control.push(this.initRulesArray());
+
+
+  searchFocused(value) {
+    if (value == '') {
+      return "SHOWING ALL HOTELS";
+    }
   }
 
+  searchQueryEntered(query) {
+    if(query.includes("SHOWING ALL HOTELS")) {
+      return query.replace("SHOWING ALL HOTELS",'');
+    }
+    return query;
+  }
+
+  addRulesForm() {
+    const control = <FormArray>this.billingForm.controls['ruleDetails']; 1
+    control.push(this.initRulesArray());
+  }
   removeRule(i) {
     console.log(this.billingForm.value.ruleDetails);
     console.log(i);
-    this.billingForm.value.ruleDetails.splice(i, 1);  
+    this.billingForm.value.ruleDetails.splice(i, 1);
     this.ngOnInit();
   }
   hotelSelected(hotel, i) {
@@ -66,12 +80,12 @@ export class BillingComponent implements OnInit {
     this.billingForm.value.ruleDetails[i].connectedHotels.splice(index, 1);
   }
   productChoice(productSelected) {
-      this.formDataService.getUsers(productSelected).subscribe(
-        res => {
-          this.hotels = JSON.parse(res["_body"]);
-          console.log(this.hotels);
-        }
-      );
+    this.formDataService.getUsers(productSelected).subscribe(
+      res => {
+        this.hotels = JSON.parse(res["_body"]);
+        console.log(this.hotels);
+      }
+    );
   }
 
   //To check if any of the payment type is tansaction based. Based on which we change the Payment type in Validity form
@@ -92,6 +106,6 @@ export class BillingComponent implements OnInit {
   }
 
 
- 
+
 
 }
