b81c232d9f4cc6ed866a5fbf853c4ea765044740
Tech <madhav.sharma@axisrooms.com>
2018-03-26 14:08:21 +0530

    UI Modifications and enhancements

--- node-server/public/supplier.module.chunk.js
+++ node-server/public/supplier.module.chunk.js
@@ -331,7 +331,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, ":host{\n    display: inline-block;\n    width: 25%;\n    background: #006bab;\n}", ""]);
+exports.push([module.i, ":host{\n    display: inline-block;\n    width: 25%;\n    background: #006bab;\n}\n", ""]);
 
 // exports
 
