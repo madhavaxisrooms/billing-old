473c24be6da445ae085c36079ff36846b44abe52
Madhav <madhav.sharma@axisrooms.com>
2018-03-29 12:50:38 +0530

    Search by Product ID enabled in billing form

--- src/app/shared/pipes/search.pipe.ts
+++ src/app/shared/pipes/search.pipe.ts
@@ -7,17 +7,16 @@ export class SearchPipe implements PipeTransform {
 
   transform(values: any, query: string): any {
     let filteredList = [];
-
-    if(query == "SHOWING ALL HOTELS"){
+    if (query == "SHOWING ALL HOTELS") {
       return values;
     }
-
     if (query != '') {
       query = query.toUpperCase();
-      for (let i = 0, len = values.length; i < len; i++)
-        if (values[i].productName.toUpperCase().indexOf(query) != -1) filteredList.push(values[i]);
+      for (let i = 0, len = values.length; i < len; i++) {
+        let jointSearch = values[i].productName + values[i].productId;
+        if (jointSearch.toUpperCase().indexOf(query) != -1) filteredList.push(values[i]);
+      }
     }
-
     return filteredList;
   }
 
